38197f7a5be89c1b8826fdbb4c19abff640f1dde
Jtiser <julien.tiser@outlook.com>
2018-01-20 15:26:08 +0100

    Using a Special Lifecycle Hook and playing with Promises

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () => (
-    <div>
-        <h1>The Main Index Page</h1>
-        <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-    </div>
-);
+class IndexPage extends Component {
+    //One way to use this :
+    // static async getInitialProps(context) {
+    //     console.log(context); 
+    //     await
+    //     return { appName: 'Super App' };
+    // }
 
-export default indexPage;
+    //The other way to use it:
+    static getInitialProps(context) {
+        console.log(context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve({ appName: 'Super App' });
+            }, 1000);
+        });
+        return promise;
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>The Main Page of {this.props.appName}.</h1>
+                <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+                <button onClick={() => Router.push('/auth')}>Go to Auth</button>
+            </div>
+        );
+    }
+}
+
+export default IndexPage;
