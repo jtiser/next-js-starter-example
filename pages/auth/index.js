96622b7bcfd377f76e01f3f269514df800407a84
Jtiser <julien.tiser@outlook.com>
2018-01-20 15:09:31 +0100

    Added a Component to experiment its rendering

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
+import User from '../../components/Users';
+
 const authPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
+        <User name="Julien" age={31} />
     </div>
 );
 
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const indexPage = () => (
     <div>
         <h1>The Main Index Page</h1>
         <p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth')}>Go to Auth </button>
+        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
     </div>
 );
 
