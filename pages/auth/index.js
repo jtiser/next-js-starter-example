7bd648f86d7d908f1a754eb754e7ff38de828c9f
Jtiser <julien.tiser@outlook.com>
2018-01-20 14:59:48 +0100

    Setting up my first Pages

---
+++ pages/auth/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const authPage = () => (
+    <div>
+        <h1>The Auth Index Page</h1>
+    </div>
+);
+
+export default authPage;
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const indexPage = () => (
+    <div>
+        <h1>The Main Index Page</h1>
+    </div>
+);
+
+export default indexPage;
